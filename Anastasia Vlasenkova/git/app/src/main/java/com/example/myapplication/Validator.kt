06d5a2336a8fecc4c9f7e6ab3c9b392088294133
Vlasenkova <yfcnzvfcz@mail.ru>
2021-11-09 23:04:37 +0300

    add a void check.

--- Anastasia Vlasenkova/git/app/src/main/java/com/example/myapplication/Validator.kt
+++ Anastasia Vlasenkova/git/app/src/main/java/com/example/myapplication/Validator.kt
@@ -4,27 +4,33 @@ import android.content.Context
 
 class Validator(private val context: Context) {
     fun checkEmail(email: String): String? {
-        return if ((email.length < 6) or (!(email.contains("@"))))
-            context.getString(R.string.email_error)
-        else
-            null
+        return when {
+            (email.isBlank()) -> context.getString(R.string.error_empty)
+            ((email.length < 6) or (!(email.contains("@")))) ->
+                context.getString(R.string.email_error)
+            else -> null
+        }
     }
     fun checkPassword(pass: String): String? {
-        return if (pass.length < 8)
-            context.getString(R.string.password_error)
-        else
-            null
+        return when {
+            (pass.isBlank()) -> context.getString(R.string.error_empty)
+            (pass.length < 8) -> context.getString(R.string.password_error)
+            else -> null
+        }
     }
     fun checkPasswordEquality(pass: String, repeat_pass: String): String? {
-        return if (pass != repeat_pass)
-            context.getString(R.string.error_confirm_password)
-        else
-            null
+        return when {
+            (repeat_pass.isBlank()) -> context.getString(R.string.error_empty)
+            ((pass != repeat_pass) or (repeat_pass.isEmpty())) ->
+                context.getString(R.string.error_confirm_password)
+            else -> null
+        }
     }
     fun checkName(name: String): String? {
-        return if (name.length < 4)
-            context.getString(R.string.error_empty_name)
-        else
-            null
+        return when {
+            (name.isBlank()) -> context.getString(R.string.error_empty)
+            (name.length < 4) -> context.getString(R.string.error_empty_name)
+            else -> null
+        }
     }
 }
