edcbe834a6f9ecfe693c2f67eecf45b690f7a530
Dinar Churakov <68560072+DyreLosh@users.noreply.github.com>
2022-01-20 09:43:57 +0300

    Churakov-L7-8 (#21)
    * Added transitions between Activity
    
    * Delete filler.txt
    
    * - Fixed bugs
    - Added validation in the class
    
    * Update readme
    
    * Update reviewdog.yml
    
    * - Fixed bugs
    
    * - Fixed bugs
    
    * - Fix bugs
    
    * - Fix bugs
    
    * - Fix bugs
    
    * Fix check code errors
    
    * Fix check code errors
    
    * Fix check code errors
    
    * Fix check code errors
    
    * Fix check code errors
    
    * Fix check code errors
    
    * Fix check code errors
    
    * Fix check code errors
    
    * Fix check code errors
    
    * Fix check code errors
    
    * Fix bugs/errors
    
    * Fix bugs/errors
    
    * Fix
    
    * Fix
    
    * Fix
    
    * Fix
    
    * -Add Fragment
    -Add Navigation
    
    * -Add Fragment
    -Add Navigation
    
    * -Add Fragment
    -Add Navigation
    
    * Delete .idea
    
    * Change .gitignore
    
    * Change ProfileActivity.kt
    
    * Change ProfileActivity.kt
    
    * Change ProfileActivity.kt
    
    * .
    
    * add Toolbar and ViewPager
    
    * Fix conflicts
    
    * Fix conflicts
    
    * Fix conflicts
    
    * Fix bug
    
    * Fix bug
    
    * Add design RecyclerView
    
    * -Add RecyclerView
    -Add FAB
    -Add design for RecyclerView
    
    * -removed conflicts
    
    * Correct lint errors
    
    * Correct lint errors
    
    * Correct lint errors
    
    * Correct lint errors
    
    * Correct lint errors
    
    * Correct lint errors
    
    * Correct lint errors
    
    * Correct lint errors
    
    * Resolved conversations
    
    * Resolved conversations
    
    * Fix bugs
    
    * Fix bugs
    
    * Fix bugs
    
    * Fix Task.kt
    
    * Add Toolbar and NavigationDrawer
    
    * Add Toolbar and NavigationDrawer
    
    Co-authored-by: Konstantin Goncharov <kostjag15@gmail.com>
    Co-authored-by: Konstantin Goncharov <51877618+MegaR1CK@users.noreply.github.com>

--- Dinar Churakov/Churakov_L3/app/src/main/java/com/example/design_log/ProfileActivity.kt
+++ Dinar Churakov/Churakov_L3/app/src/main/java/com/example/design_log/ProfileActivity.kt
@@ -1,11 +1,15 @@
 package com.example.design_log
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.setupWithNavController
 import com.example.design_log.databinding.ActivityProfileBinding
-import com.example.design_log.fragments.ProfileFragment
-import com.example.design_log.fragments.TasksFragment
 
 class ProfileActivity : AppCompatActivity() {
 
@@ -13,23 +17,42 @@ class ProfileActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         val binding = ActivityProfileBinding.inflate(layoutInflater)
         setContentView(binding.root)
-
-        binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
-            when (menuItem.itemId) {
-                R.id.menuHome -> {
-                    supportBottomNavigation(TasksFragment())
-                }
-                R.id.menuProfile -> {
-                    supportBottomNavigation(ProfileFragment())
-                }
-            }
-            true
-        }
+        setSupportActionBar(binding.toolbarTask)
+        addEmailNameToMenu(binding)
+        onNavigateOptionsMenu(binding)
     }
-    private fun supportBottomNavigation(fragment: Fragment) {
-        supportFragmentManager
-            .beginTransaction()
-            .replace(R.id.fragmentContainer, fragment)
-            .commit()
+
+    private fun onNavigateOptionsMenu(binding: ActivityProfileBinding) {
+
+        val navHostFragment = supportFragmentManager
+            .findFragmentById(R.id.fragmentContainer) as NavHostFragment
+        val navController = navHostFragment.navController
+        val builder = AppBarConfiguration.Builder(navController.graph)
+        builder.setOpenableLayout(binding.profileDrawerLayout)
+        val appBarConfiguration = builder.build()
+        binding.toolbarTask.setupWithNavController(navController, appBarConfiguration)
+        NavigationUI.setupWithNavController(binding.profileMenu, navController)
+    }
+
+    private fun addEmailNameToMenu(binding: ActivityProfileBinding) {
+
+        val headerEmailText: TextView = binding.profileMenu.getHeaderView(0)
+            .findViewById(R.id.headerEmailText)
+        val name = intent.getStringExtra("Name")
+        headerEmailText.text = getString(R.string.header_email_text, name)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.profile_toolbar_menu, menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.exitMenu -> {
+                finish()
+            }
+        }
+        return true
     }
 }
