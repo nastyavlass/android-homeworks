87bbb3c48cc672cd5fdf67fcc683cf953864b5fa
Vlasenkova <yfcnzvfcz@mail.ru>
2022-03-03 00:08:27 +0300

    fix the text.

--- Anastasia Vlasenkova/git/app/src/main/java/com/example/myapplication/TasksListHomeFragment.kt
+++ Anastasia Vlasenkova/git/app/src/main/java/com/example/myapplication/TasksListHomeFragment.kt
@@ -1,6 +1,5 @@
 package com.example.myapplication
 
-import android.content.Intent
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
@@ -10,7 +9,6 @@ import androidx.annotation.RequiresApi
 import androidx.fragment.app.Fragment
 import com.example.myapplication.databinding.FragmentHomeTasksListBinding
 import com.google.android.material.tabs.TabLayoutMediator
-import java.time.LocalDateTime
 
 class TasksListHomeFragment : Fragment() {
     lateinit var binding: FragmentHomeTasksListBinding
